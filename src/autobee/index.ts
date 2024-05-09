8f8469f5cb548d179dfc4259b3fbe7ba20333cf7
Alexander Semyenov <semyenov@icloud.com>
2024-05-09 09:47:22 +0300

    .

--- src/autobee/index.ts
+++ src/autobee/index.ts
@@ -1,34 +1,32 @@
-import Autobase, { AutobaseHandlers } from 'autobase'
-import b4a from 'b4a'
-import Corestore from 'corestore'
-import Hyperbee, { HyperbeeRange } from 'hyperbee'
+import Autobase, { AutobaseHandlers } from "autobase"
+import b4a from "b4a"
+import Corestore from "corestore"
+import Hyperbee, { HyperbeeRange } from "hyperbee"
 
 type Op = {
-  type: 'del' | 'put' | 'add' | 'exit'
+  type: "del" | "put" | "add" | "exit"
   key: string
   value?: any
   opts?: any
 }
 
 export default class Autobee extends Autobase<Hyperbee, Op> {
-  constructor(...args: any[]) {
-    let [store, bootstrap, handlers] = args as [
-      Corestore,
-      string | Buffer | null,
-      AutobaseHandlers<Hyperbee>,
-    ]
-
+  constructor(...[store, bootstrap, handlers]: [
+    Corestore,
+    string | Buffer | null,
+    AutobaseHandlers<Hyperbee>,
+  ]) {
     handlers = {
       apply: Autobee.apply,
 
       ...handlers,
-      valueEncoding: 'json',
+      valueEncoding: "json",
       open: (store: Corestore) => {
-        const core = store.get('autobee')
+        const core = store.get("autobee")
         return new Hyperbee(core, {
           extension: false,
-          valueEncoding: 'json',
-          keyEncoding: 'utf-8',
+          valueEncoding: "json",
+          keyEncoding: "utf-8",
         })
       },
     }
@@ -41,12 +39,13 @@ export default class Autobee extends Autobase<Hyperbee, Op> {
     view: Hyperbee,
     base: Autobase<Hyperbee, Op>,
   ) {
+    await base.ready()
     const b = view.batch({ update: false })
 
     for (const node of batch) {
       const { type, key, value, opts } = node.value
       console.log(
-        '\nCurrent operation\n',
+        "\nCurrent operation\n",
         JSON.stringify(
           {
             type,
@@ -60,10 +59,10 @@ export default class Autobee extends Autobase<Hyperbee, Op> {
       )
 
       switch (type) {
-        case 'put':
+        case "put":
           await b.put(key, value, opts)
           break
-        case 'del':
+        case "del":
           await b.del(key, opts)
           break
       }
@@ -74,7 +73,7 @@ export default class Autobee extends Autobase<Hyperbee, Op> {
 
   async put<T = any>(key: string, value: T, opts: any = {}) {
     return this.append({
-      type: 'put',
+      type: "put",
       key,
       value,
       opts,
@@ -83,7 +82,7 @@ export default class Autobee extends Autobase<Hyperbee, Op> {
 
   async del(key: string, opts: any = {}) {
     return this.append({
-      type: 'del',
+      type: "del",
       key,
       value: null,
       opts,
@@ -91,7 +90,7 @@ export default class Autobee extends Autobase<Hyperbee, Op> {
   }
 
   async get<T = any>(key: string, opts: any = {}) {
-    return this.view.get<T>(b4a.from(key, 'hex'), opts)
+    return this.view.get<T>(b4a.from(key, "hex"), opts)
   }
 
   async peek<T = any>(range?: HyperbeeRange, opts: any = {}) {
--- src/autobee/test.ts
+++ src/autobee/test.ts
@@ -1,17 +1,17 @@
-import b4a from 'b4a'
-import readline from 'readline'
-import Hyperswarm from 'hyperswarm'
-import Corestore from 'corestore'
-import Hyperbee from 'hyperbee'
-import Autobase from 'autobase'
-import Autobee from './index'
+import b4a from "b4a"
+import readline from "readline"
+import Hyperswarm from "hyperswarm"
+import Corestore from "corestore"
+import Hyperbee from "hyperbee"
+import Autobase from "autobase"
+import Autobee from "./index"
 
 const args = process.argv.slice(2)
-const storageDir = args[1] ?? './storage'
+const storageDir = args[1] ?? "./storage"
 
 async function addWriter(db: Autobee, key: string) {
   await db.append({
-    type: 'add',
+    type: "add",
     key,
   })
 
@@ -28,15 +28,15 @@ const store = new Corestore(storageDir)
 await store.ready()
 
 const bootstrap = args[0]
-console.log('bootstrap', bootstrap)
+console.log("bootstrap", bootstrap)
 
 const db = new Autobee(store.session(), bootstrap, {
   apply: async (batch: any[], view: Hyperbee, base: Autobase<Hyperbee>) => {
     for (const node of batch) {
       const { type, key } = node.value
-      if (type === 'add') {
-        console.log('\rAdding writer', key)
-        await base.addWriter(b4a.from(key, 'hex'), {
+      if (type === "add") {
+        console.log("\rAdding writer", key)
+        await base.addWriter(b4a.from(key, "hex"), {
           indexer: true,
         })
       }
@@ -47,12 +47,12 @@ const db = new Autobee(store.session(), bootstrap, {
 })
 await db.ready()
 
-db.view.core.on('append', async () => {
+db.view.core.on("append", async () => {
   // Skip append event for hyperbee's header block
   if (db.view.version === 1) return
   rl.pause()
 
-  console.log('\nCurrent db key/value pairs')
+  console.log("\nCurrent db key/value pairs")
   for await (const node of db.createReadStream<{
     key: Buffer
     value: any
@@ -64,14 +64,14 @@ db.view.core.on('append', async () => {
 })
 
 if (!bootstrap) {
-  console.log('db.key', b4a.toString(db.local.key, 'hex'))
+  console.log("db.key", b4a.toString(db.local.key, "hex"))
 }
 
 const swarm = new Hyperswarm()
-swarm.on('connection', (connection, peerInfo) => {
+swarm.on("connection", (connection, peerInfo) => {
   rl.pause()
 
-  console.log('\nPeer joined', b4a.toString(peerInfo.publicKey, 'hex'))
+  console.log("\nPeer joined", b4a.toString(peerInfo.publicKey, "hex"))
 
   rl.prompt()
   db.store.replicate(connection)
@@ -81,47 +81,47 @@ const discovery = swarm.join(b4a.toBuffer(db.discoveryKey))
 await discovery.flushed()
 
 rl.pause()
-console.log('putting a key')
+console.log("putting a key")
 
 const simplePut = async (
   db: Autobee,
-  key: string = 'test/default',
-  value = 'hello',
+  key: string = "test/default",
+  value = "hello",
   opts: any = {},
 ) => {
-  console.log('\nWriter', b4a.toString(db.local.key, 'hex'))
+  console.log("\nWriter", b4a.toString(db.local.key, "hex"))
   await db.put(key, value, opts)
 }
 if (db.writable) {
   await simplePut(db)
 } else {
-  console.log('\nDB isnt writable yet')
-  console.log('have another writer add the following key')
-  console.log('key', b4a.toString(db.local.key, 'hex'))
+  console.log("\nDB isnt writable yet")
+  console.log("have another writer add the following key")
+  console.log("key", b4a.toString(db.local.key, "hex"))
 }
 
 console.log(`\nEnter "add <key>" to add a writer.
 Otherwise enter 'exit' to exit.`)
 
-rl.on('line', async (line) => {
+rl.on("line", async (line) => {
   if (!line) {
     rl.prompt()
     return
   }
   rl.pause()
 
-  const [type, key, value, opts]: string[] = line.split(' ')
+  const [type, key, value, opts]: string[] = line.split(" ")
   switch (type) {
-    case 'put':
+    case "put":
       await simplePut(db, key, value, opts)
       break
-    case 'del':
+    case "del":
       await db.del(key, opts)
       break
-    case 'add':
+    case "add":
       await addWriter(db, key)
       break
-    case 'exit':
+    case "exit":
       rl.close()
       await db.close()
       await store.close()
